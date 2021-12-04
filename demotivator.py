2047917394602ad75baddca251f0dda8649f773f
Cub1tor <ostashenkokd@gmail.com>
2021-12-04 21:04:21 +0300

    different files

---
+++ demotivator.py
@@ -0,0 +1,42 @@
+import io
+import random
+
+from PIL import Image, ImageDraw, ImageFont
+
+default_captions = [
+    "MAN",
+    "Какой ты сегодня?"
+    "ДАЙТЕ УД\nДАЙТЕ УД\nДАЙТЕ УД",
+    "Are ya winning, son?"
+    "Мальчик, водочки нам принеси\nМы МГУ закончили",
+    "КАК БРАТА ТЕБЯ ПРОШУ\nВЫПРЯМИ СПИНУ",
+    "ум ваче сей(("
+]
+
+
+def create_demotivator(image: bytes, caption: str = None):
+
+    if caption is None:
+        caption = random.choice(default_captions)
+
+    image_stream = io.BytesIO(image)
+    photo = Image.open(image_stream)
+    photo_w, photo_h = photo.size
+
+    bg_w, bg_h = round(1.35*photo_w), round(1.35*photo_h) + 100
+    background = Image.new('RGB', (bg_w, bg_h), color=(0, 0, 0))
+
+    paste_w, paste_h = ((bg_w - photo_w) // 2, (bg_h - photo_h) // 2 - 80)
+    background.paste(photo, (paste_w, paste_h))
+
+    draw = ImageDraw.Draw(background)
+    rectangle_w, rectangle_h = (paste_w + photo_w + 8, paste_h + photo_h + 8)
+    draw.rectangle([(paste_w - 8, paste_h - 8), (rectangle_w, rectangle_h)],
+                   fill=None, outline=(255, 255, 255), width=3)
+
+    fnt = ImageFont.truetype('/usr/share/fonts/liberation/LiberationSerif-Regular.ttf', 50)
+    draw.multiline_text((bg_w // 2, rectangle_h + 20), text=caption, anchor="ma", align="center", font=fnt, fill=None)
+
+    background.save("img.jpg")
+    fp = open("img.jpg", 'rb')
+    return fp
--- first_bot.py
+++ first_bot.py
@@ -1,8 +1,6 @@
-import os
-
 from config import TOKEN
 from telebot import TeleBot, types
-from PIL import Image, ImageDraw, ImageFont
+from demotivator import create_demotivator
 
 bot = TeleBot(TOKEN, parse_mode="MarkdownV2")
 
@@ -30,35 +28,14 @@ def meme_command(msg: types.Message):
 
 def receive_image_and_caption(msg: types.Message):
     if msg.photo is None:
-        bot.send_message(msg.chat.id, "Ты не отправил фото, а зря! Попробуй еще раз")
-        bot.register_next_step_handler_by_chat_id(msg.chat.id, receive_image_and_caption)
-    elif msg.caption is None:
-        bot.send_message(msg.chat.id, "Ты не отправил подпись, а зря! Попробуй еще раз")
+        bot.send_message(msg.chat.id, "Ты не отправил фото, а зря\\! Попробуй еще раз")
         bot.register_next_step_handler_by_chat_id(msg.chat.id, receive_image_and_caption)
     else:
+        if msg.caption is None:
+            bot.send_message(msg.chat.id, "Ты не отправил подпись, а зря\\! Придётся придумать её за тебя")
         file_info = bot.get_file(msg.photo[-1].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        with open("img.jpg", 'wb') as fp:
-            fp.write(downloaded_file)
-        create_demotivator(msg.chat.id, msg.caption)
-
-
-def create_demotivator(chat_id: int, caption: str):
-    photo = Image.open("img.jpg", "r")
-    photo_w, photo_h = photo.size
-    bg_w, bg_h = round(1.35*photo_w), round(1.35*photo_h) + 100
-    background = Image.new('RGB', (bg_w, bg_h), color=(0, 0, 0))
-    paste_w, paste_h = ((bg_w - photo_w) // 2, (bg_h - photo_h) // 2 - 80)
-    background.paste(photo, (paste_w, paste_h))
-    draw = ImageDraw.Draw(background)
-    rectangle_w, rectangle_h = (paste_w + photo_w + 8, paste_h + photo_h + 8)
-    draw.rectangle([(paste_w - 8, paste_h - 8), (rectangle_w, rectangle_h)], fill=None, outline=(255, 255, 255), width=3)
-    fnt = ImageFont.truetype('/usr/share/fonts/liberation/LiberationSerif-Regular.ttf', 50)
-    draw.text((bg_w // 2, rectangle_h + 20), text=caption, anchor="ma", font=fnt, fill=None)
-    background.save('img.jpg')
-    with open("img.jpg", 'rb') as fp:
-        bot.send_photo(chat_id, fp)
-        os.unlink("img.jpg")
+        bot.send_photo(msg.chat.id, create_demotivator(downloaded_file, msg.caption))
 
 
 if __name__ == '__main__':
